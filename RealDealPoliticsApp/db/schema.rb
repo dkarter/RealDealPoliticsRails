4361a84fc48d81e069638aa8a26f23f74b50e42d
Dorian Karter <dkarter@gmail.com>
2014-02-08 12:43:26 -0600

    Finally got the user model working with bootstrap

--- RealDealPoliticsApp/db/schema.rb
+++ RealDealPoliticsApp/db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140208183046) do
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "zipcode"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
